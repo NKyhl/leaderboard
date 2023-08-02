a7743dca2b1029078236509e51a58f0fb6440a28
Nolan Kyhl <nolan@kyhl.com>
2023-08-01 21:12:28 -0500

    adjust for time zones

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const root = document.documentElement;
 
-// Set the date we're counting down to
-var countDownDate = new Date("Aug 28, 2023 10:00:00").getTime();
+const countDownDate = new Date("Aug 28, 2023 22:00:00 UTC-5").getTime();
+const updateclock = setInterval(function() {
 
-// Update the count down every 1 second
-var updateclock = setInterval(function() {
+  // Get user's date and time in Eastern
+  const localNow = new Date().toLocaleString('en-US', {timeZone: 'America/New_York'});
+  const easternNow = new Date(localNow)
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  // Time calculations for difference
+  const difference = countDownDate - easternNow;
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
+  // Display the result
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
+  // If the count down is finished
+  console.log(difference) 
+  if (difference < 0) {
+    clearInterval(updateclock);
     document.getElementById("countdown").innerHTML = "IN PROGRESS";
   }
 }, 1000);
